e8e7daf0fdabcba5f88b4b22782288b66d7925bc
Maylor-tech <maylortech007@gmail.com>
2025-10-12 13:58:10 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE || '/bornfidis-funnel/',
+  base: process.env.VITE_BASE || '/bornfidis-funnel/',   // <-- important
 })
+
 
