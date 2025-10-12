85ae693cf68f82ae551fe41a336672f965614ed2
Maylor-tech <maylortech007@gmail.com>
2025-10-12 13:24:28 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// IMPORTANT for GitHub Pages: use /<repo>/
 export default defineConfig({
   plugins: [react()],
   base: process.env.VITE_BASE || '/bornfidis-funnel/',
 })
+
